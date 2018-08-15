846b0ebbb3997f7520d1ffb70d2fde198caa754b
Valentin Brosch <brosch@sternsinger.de>
2018-08-15 16:50:33 +0200

    added probability estimates

--- histogram.py
+++ histogram.py
@@ -49,9 +49,13 @@ def plot_bins():
     plt.bar(x_bins, densities, delta, color='#0B3861', edgecolor='#000000', linewidth=1)
 
 
+def bin_probabilities():
+    return [(b[1] * delta) for b in bins]
+
+
 samples = generate_set()
 
-for i, dx in enumerate([0.25, 0.5, 1, 2, 3, 4]):
+for i, dx in enumerate([0.25, 0.5, 1, 2, 4, 5]):
     plt.subplot(2, 3, i + 1)
     plt.title("$\Delta = {}$".format(dx))
 
@@ -60,5 +64,12 @@ for i, dx in enumerate([0.25, 0.5, 1, 2, 3, 4]):
     plot_pdf()
     bins = get_bins()
     plot_bins()
+
+    bin_probs = bin_probabilities()
+    print("Bin probabilities for d={}: {}".format(dx, bin_probs))
+
+    sum_probs = sum(bin_probs)
+    print("Is bin probability for d={} valid probability distribution: {} ({})".format(dx, math.isclose(sum_probs, 1),
+                                                                                       sum_probs))
 
 plt.show()
